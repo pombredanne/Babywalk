f3b2e22a0efad9d3f1b92b813d631b8ff1ae1157
Laszlo Nagy <rizsotto@gmail.com>
2015-10-22 21:28:34 +0000

    simplify fetch logic by introduce new request type

--- libbabywalk/fetch.py
+++ libbabywalk/fetch.py
@@ -1,71 +1,60 @@
 import os
 import os.path
-import shutil
 import boto3
 import logging
 import subprocess
-import uuid
+import tempfile
 
 
-class Content(object):
-    def __init__(self, directory, uid):
-        self.name = os.path.join(directory, uid)
-        self.content = os.path.join(self.name, 'content')
-        self.result = os.path.join(self.name, 'result.warc.gz')
-        os.mkdir(self.name)
+def _fetch(request, working_dir):
 
-    def __enter__(self):
-        return self
+    depth = request['depth']
+    seed = request['url']
 
-    def __exit__(self, _type, _value, _traceback):
-        if self.name is not None:
-            shutil.rmtree(self.name)
+    exts = {
+        '.js', '.css', '.xml', '.atom', '.rss', '.jpeg', '.jpg', '.tif',
+        '.gif', '.bmp', '.png', '.svg', '.ttf', '.woff', '.psb', '.psd',
+        '.pdf', '.gz', '.bz2', '.zip', '.doc', '.docx', '.xls', '.xslx',
+        '.ppt', '.pptx', '.swf', '.flac', '.fla', '.mp3', '.mp4', '.wav',
+        '.wmv', '.mov'
+    }
 
-    def fetch(self, seed, depth):
-        exts = {
-            '.js', '.css', '.xml', '.atom', '.rss', '.jpeg', '.jpg', '.tif',
-            '.gif', '.bmp', '.png', '.svg', '.ttf', '.woff', '.psb', '.psd',
-            '.pdf', '.gz', '.bz2', '.zip', '.doc', '.docx', '.xls', '.xslx',
-            '.ppt', '.pptx', '.swf', '.flac', '.fla', '.mp3', '.mp4', '.wav',
-            '.wmv', '.mov'
-        }
+    cmd = ['wget', '--recursive', '--level={}'.format(depth), '--tries=5',
+           '--dns-timeout=30', '--connect-timeout=5', '--read-timeout=5',
+           '--timestamping', '--wait=5', '--random-wait', '--no-parent',
+           '--no-verbose', '--no-check-certificate',
+           '--reject=' + ','.join(exts),
+           '--reject=' + ','.join(ext.upper() for ext in exts),
+           '--warc-file=result', '--warc-tempdir=.', seed]
 
-        cmd = ['wget', '--recursive', '--level={}'.format(depth), '--tries=5',
-               '--dns-timeout=30', '--connect-timeout=5', '--read-timeout=5',
-               '--timestamping', '--wait=5', '--random-wait', '--no-parent',
-               '--no-verbose', '--no-check-certificate',
-               '--reject=' + ','.join(exts),
-               '--reject=' + ','.join(ext.upper() for ext in exts),
-               '--warc-file', os.path.join(self.name, 'result'),
-               '--warc-tempdir=.', seed]
+    logging.debug('execute %s', cmd)
+    ec = subprocess.call(cmd,
+                         cwd=working_dir,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    logging.info('finished crawling %s with %s', cmd[-1], ec)
+    return os.path.join(working_dir, 'result.warc.gz')
 
-        logging.debug('execute %s', cmd)
-        ec = subprocess.call(cmd,
-                             cwd=self.content,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
-        logging.info('finished crawling %s with %s', cmd[-1], ec)
 
-    def upload(self, bucket, filename):
-        if not os.path.exists(self.result):
-            return None
+def _upload(request, warc_file):
 
-        with open(result_path, mode='rb') as handle:
-            boto3.resource('s3') \
-                 .Bucket(bucket) \
-                 .put_object(Key=filename,
-                             ContentType='application/x-gzip',
-                             Body=handle.read())
+    if not os.path.exists(warc_file):
+        return None
 
-        return 's3://{}/{}'.format(bucket, filename)
+    with open(warc_file, mode='rb') as handle:
+        boto3.resource('s3') \
+             .Bucket(request['bucket']) \
+             .put_object(Key=request['object'],
+                         ContentType='application/x-gzip',
+                         Body=handle.read())
 
-    @staticmethod
-    def get(opts):
+    return 's3://{}/{}'.format(request['bucket'], request['object'])
 
-        for url in opts['seeds']:
-            uid = uuid.uuid3(uuid.NAMESPACE_URL, url)
-            with Content(opts['directory'], uid) as temp:
-                temp.fetch(url, opts['depth'])
-                objectname = uuid + '.' + opts['tag'] + '.warc.gz'
-                result = temp.upload(opts['bucket'], objectname)
-                yield {'seed': url, 'location': result}
+
+def fetch_and_upload(requests, directory):
+
+    for request in requests:
+        with tempfile.TemporaryDirectory(dir=directory) as tmpdir:
+            warcfile = _fetch(request['fetch'], tmpdir)
+            result = _upload(request['upload'], warcfile)
+            yield (request, result)
