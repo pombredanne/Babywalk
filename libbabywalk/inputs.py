59f452a511218a36d90b11f87d1e193144a80326
Laszlo Nagy <laszlo.3.nagy@here.com>
2015-11-17 18:07:21 +0100

    fix seed program

--- libbabywalk/inputs.py
+++ libbabywalk/inputs.py
@@ -1,30 +1,17 @@
+import re
 import csv
-import urllib.parse
+import boto3
+import codecs
 
 
-def _rows_from_tsv(filename):
+def read_seeds_from_s3(path, filename):
 
-    with open(filename, mode='r', encoding='utf-8') as handle:
-        reader = csv.DictReader(handle, delimiter='\t')
-        for row in reader:
-            yield row
+    uri = path + '/' + filename
+    bucket, name = re.match(r's3://(.*)/(.*)', uri).groups()
 
+    response = boto3.resource('s3').Object(bucket, name).get()
+    if response:
+        codec = codecs.getreader('utf-8')
+        handle = codec(response.get('Body'), 'ignore')
 
-def _filter_ppid_and_seed(iterator):
-    def normalize(url):
-        atoms = urllib.parse.urlsplit(url)
-        if not atoms.scheme:
-            return normalize('http://' + url)
-        path = atoms.path if atoms.path else '/'
-        return urllib.parse.urlunsplit((atoms.scheme, atoms.netloc, path,
-                                        atoms.query, atoms.fragment))
-
-    return ({
-        'url': normalize(item['homepage']),
-        'ppid': item['place_id']
-    } for item in iterator)
-
-
-def read_seeds_from_file(filename):
-
-    return _filter_ppid_and_seed(_rows_from_tsv(filename))
+        return (row for row in csv.DictReader(handle, delimiter='\t'))
